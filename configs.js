960076af9000008ff4902c77e4fb6aa2495a01c6
象跑跑 <xiangpaopao@gmail.com>
2014-08-21 17:18:04 +0800

    ignore

---
+++ configs.js
@@ -0,0 +1,22 @@
+var airpubConfigs = {
+  // blog name
+  name: '插屁屁',
+  // just a little description
+  description: '插屁屁提高班',
+  // blog uri
+  url: 'http://xiangpaopao.github.io/xiangpaopao/',
+  // blog theme
+  // theme: 'my-theme',
+  // for images upload
+  upyun: {
+    bucket: 'chapipi',
+    uri: 'http://chapipi.b0.upaiyun.com',
+    endpoint: 'http://v0.api.upyun.com/chapipi',
+    form_api_secret: 'KDlTgI8ZES/ZYOiyq5qdiCzIIOU='
+  }
+}
+
+// for backend service
+var duoshuoQuery = {
+  short_name: 'xpp'
+};
